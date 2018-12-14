11c5cc46d1d44bd30c41189b1b5845e8f73d9240
Mislav Hećimović <mislav.hecimovic@gmail.com>
2018-12-14 15:51:59 +0100

    Further project architecture development Removed .NET Framework project, added dotnetcore console app

---
+++ RClient/Client.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.Redis;
+
+namespace RClient
+{
+    class Client
+    {
+
+        public string Test()
+        {
+            var manager = new RedisManagerPool("localhost:6379");
+            using (var client = manager.GetClient())
+            {
+                client.Set("foo", "bar");
+                return $"foo={client.Get<string>("foo")}";
+            }
+        }
+    }
+}
